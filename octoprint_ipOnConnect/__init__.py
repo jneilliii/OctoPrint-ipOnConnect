ddec2940bb15022d7163e4221195228caba5d1ca
jneilliii <jneilliii+github@gmail.com>
2016-09-24 18:35:32 -0400

    fixing indentation.

--- octoprint_ipOnConnect/__init__.py
+++ octoprint_ipOnConnect/__init__.py
@@ -19,8 +19,8 @@ class ipOnConnectPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.EventHan
 		
 	##-- EventHandler hook 
 	def on_event(self, event, payload):
-        if event == "Connected":
-            self._printer.command("M117 " + [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+		if event == "Connected":
+			self._printer.command("M117 " + [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
 
 	##~~ Softwareupdate hook
 
